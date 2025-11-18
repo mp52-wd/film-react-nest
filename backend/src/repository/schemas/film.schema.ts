09e3e5fe1f36e9bfd26e928390905a73d02bfd1c
Shelepov Sergey <shelepov-sa@yandex.ru>
2025-11-18 11:44:16 +0300

    feat: добавл схемы Mongoose для фильмов

---
+++ backend/src/repository/schemas/film.schema.ts
@@ -0,0 +1,61 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ _id: false })
+export class Session {
+  @Prop({ required: true })
+  id: string;
+
+  @Prop({ required: true })
+  daytime: string;
+
+  @Prop({ required: true })
+  hall: number;
+
+  @Prop({ required: true })
+  rows: number;
+
+  @Prop({ required: true })
+  seats: number;
+
+  @Prop({ required: true })
+  price: number;
+
+  @Prop({ type: [String], default: [] })
+  taken: string[];
+}
+
+@Schema({ collection: 'films' })
+export class Film extends Document {
+  @Prop({ required: true })
+  id: string;
+
+  @Prop({ required: true })
+  rating: number;
+
+  @Prop({ required: true })
+  director: string;
+
+  @Prop({ type: [String], required: true })
+  tags: string[];
+
+  @Prop({ required: true })
+  image: string;
+
+  @Prop({ required: true })
+  cover: string;
+
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  about: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ type: [Session], default: [] })
+  schedule: Session[];
+}
+
+export const FilmSchema = SchemaFactory.createForClass(Film);
