e5d31ec797fe05e5f765d739424b69f16fb26e0f
Shelepov Sergey <shelepov-sa@yandex.ru>
2025-11-19 09:09:29 +0300

    feat: DTO для заказов

--- backend/src/order/dto/order.dto.ts
+++ backend/src/order/dto/order.dto.ts
@@ -1,1 +1,25 @@
-//TODO реализовать DTO для /orders
+import { IsString, IsArray, ValidateNested, IsNumber, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class TicketDto {
+  @IsNumber()
+  @Min(1)
+  row: number;
+
+  @IsNumber()
+  @Min(1)
+  seat: number;
+}
+
+export class CreateOrderDto {
+  @IsString()
+  filmId: string;
+
+  @IsString()
+  sessionId: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => TicketDto)
+  tickets: TicketDto[];
+}
